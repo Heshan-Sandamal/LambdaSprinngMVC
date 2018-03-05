4ca0dc181c4b1a903b8043601ab72c8478691722
Heshan-Sandamal <mhsandamal1@gmail.com>
2018-03-05 17:31:53 +0530

    Initialize the spring mvc project with SAM.yaml and serverless.yml to deploy in lambda

---
+++ src/main/java/com/sysco/test/resources/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package com.sysco.test.resources;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+
+    @ApiOperation(value = "Responds with a fixed greeting", notes ="", response = String.class)
+    @RequestMapping(value = "/v1.0/hello", method = RequestMethod.GET)
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+
+}
